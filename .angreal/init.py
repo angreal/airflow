f2a82d3692ca7db0b11f57e43acc2d29f39db80c
Dylan Storey <dylan.storey@gmail.com>
2023-02-23 23:08:47 -0500

    first stop for an airflow template

---
+++ .angreal/init.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+from angreal.integrations.git import Git
+from angreal.integrations.venv import VirtualEnv
+
+
+def init():
+    os.chdir("airflow-provider-this-provider")
+    VirtualEnv(".venv", now=True, requirements=".[dev]")
+    g = Git()
+    g.init()
+    g.add('.')
+
+    subprocess.run(
+        (
+        "pre-commit install;"
+        "pre-commit run --all-files;"
+        "pre-commit run --all-files;"
+        ),
+        shell=True,
+    )
+
+    g.commit("-am 'airflow-provider-this-provider initialized via angreal'")
